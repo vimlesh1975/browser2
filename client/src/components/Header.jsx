a7128c37d88ea029e267f87a4b7dc6c6282da8aa
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-07-17 16:31:01 +0530

    ndms2.pbns.in to http://183.82.113.8/

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
 
     const logout = () => {
         localStorage.setItem('token', '{"token":""}');
-        window.location.href = 'http://ndms2.pbns.in/pbns/dmam/home/index.php';
+        window.location.href = 'http://183.82.113.8/pbns/dmam/home/index.php';
     }
     const selectBg = e => {
         dispatch({ type: 'CHANGE_BG', payload: e.target.value })
@@ -28,14 +28,14 @@ export const Header = () => {
             <input ref={bgColorRef} type="color" value='#7092BE' onChange={e => selectBg(e)} style={{ display: 'none' }}></input>
             <nav onDoubleClick={showAndSelectColor} className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: bg }}>
                 <div style={{ width: '100%' }}>
-                    <button type="button" className="btn btn-warning btn-sm  mr-1"><a href="http://ndms2.pbns.in/pbns/dmam/home/index.php">HOME</a></button>
+                    <button type="button" className="btn btn-warning btn-sm  mr-1"><a href="http://183.82.113.8/pbns/dmam/home/index.php">HOME</a></button>
                     <div className='float-right'>
-                    
-                    <span className='mr-2'>{user.userInfo.UserName}</span>
+
+                        <span className='mr-2'>{user.userInfo.UserName}</span>
 
                         <span className='mr-2'>{user.userInfo.ViewCode}</span>
                         <span className='mr-2'>{user.userInfo.FullName}</span>
-                        <span className='mr-2'>{user.userInfo.ViewCode.toString().trim().substring(user.userInfo.ViewCode.toString().trim().length-2, user.userInfo.ViewCode.toString().trim().length-1)}</span>
+                        <span className='mr-2'>{user.userInfo.ViewCode.toString().trim().substring(user.userInfo.ViewCode.toString().trim().length - 2, user.userInfo.ViewCode.toString().trim().length - 1)}</span>
 
                         {/* <button className='btn-danger' onClick={logout}>LogOut   <ExitToAppIcon /> </button> */}
                         <button className='btn-danger' onClick={logout}>LogOut    </button>
