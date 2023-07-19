304ea40b2d60a3f5df563f6bebceef586f4fab0f
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-07-19 12:07:37 +0530

    backEndIP = process.env.REACT_APP_BACKENDHOST_IP;

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,13 @@ import { useSelector, useDispatch } from 'react-redux'
 export const Header = () => {
     const user = useSelector(state => state.userReducer.user);
     const bg = useSelector(state => state.bgReducer.bg);
+    const backEndIP = process.env.REACT_APP_BACKENDHOST_IP;
+
     const dispatch = useDispatch();
 
     const logout = () => {
         localStorage.setItem('token', '{"token":""}');
-        window.location.href = 'http://183.82.113.8/pbns/dmam/home/index.php';
+        window.location.href = backEndIP + '/pbns/dmam/home/index.php';
     }
     const selectBg = e => {
         dispatch({ type: 'CHANGE_BG', payload: e.target.value })
@@ -28,7 +30,7 @@ export const Header = () => {
             <input ref={bgColorRef} type="color" value='#7092BE' onChange={e => selectBg(e)} style={{ display: 'none' }}></input>
             <nav onDoubleClick={showAndSelectColor} className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: bg }}>
                 <div style={{ width: '100%' }}>
-                    <button type="button" className="btn btn-warning btn-sm  mr-1"><a href="http://183.82.113.8/pbns/dmam/home/index.php">HOME</a></button>
+                    <button type="button" className="btn btn-warning btn-sm  mr-1"><a href={`${backEndIP}/pbns/dmam/home/index.php`}>HOME</a></button>
                     <div className='float-right'>
 
                         <span className='mr-2'>{user.userInfo.UserName}</span>
